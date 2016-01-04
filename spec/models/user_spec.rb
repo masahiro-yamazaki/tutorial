407896908afd6c135d1b0f1618d9be8ec41991a8
masahiro-yamazaki <masahiro777.ymzk@gmail.com>
2016-01-04 15:09:14 +0900

    has_secure_password :chap6-3-4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -78,5 +79,35 @@ describe User do
       @user.password_confirmation = "mismatch"
     end
     it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before do
+      @user.save
+    end
+    let(:found_user) { User.find_by(email: @user.email) }
+  
+#    describe "with valid password" do
+#      it { should eq found_user.authenticate(@user.password) }
+#      #puts "/////////ssssss////////////////"
+#      #puts found_user.class
+#      puts @user.class
+#      #puts @user.email
+#      #puts @user.password_digest
+#      #puts found_user.password_digest
+#      #puts "/////////ssssss////////////////"
+#    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+  
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
   end
 end
