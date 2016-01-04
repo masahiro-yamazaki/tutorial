e5dadd52982ae1a5dd1c9b654af5dad446883135
masahiro-yamazaki <masahiro777.ymzk@gmail.com>
2016-01-04 18:04:54 +0900

    create users/index :chap9-3-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+  def index
+    @users = User.all
+  end
+
   def show
-    puts params[:id]
     @user = User.find(params[:id])
   end
 
