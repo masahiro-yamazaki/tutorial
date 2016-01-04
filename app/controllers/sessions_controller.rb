870cbe92652cd5209d2e3db22490c36504827e99
masahiro-yamazaki <masahiro777.ymzk@gmail.com>
2016-01-04 16:53:00 +0900

    create signin page :chap8-1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーをサインインさせ、ユーザーページ (show) にリダイレクトする。
+    else
+      flash[:error] = 'Invalid email/password combination' # 誤りあり!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
