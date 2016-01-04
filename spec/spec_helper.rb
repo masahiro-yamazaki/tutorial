331a19febb1489a18934bf91e6471e7f122b1fd0
masahiro-yamazaki <masahiro777.ymzk@gmail.com>
2016-01-04 11:35:25 +0900

    success in execute rspec :chap6

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+#require "minitest/autorun"
 
 RSpec.configure do |config|
   config.include Capybara::DSL
