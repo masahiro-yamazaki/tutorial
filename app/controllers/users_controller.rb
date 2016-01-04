54c268c45133d81a3cb6de9d054d2b9ee26a868a
masahiro-yamazaki <masahiro777.ymzk@gmail.com>
2016-01-04 18:56:39 +0900

    add destroy_action :chap9-4-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -49,8 +56,14 @@ class UsersController < ApplicationController
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 
+    # Before actions
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    # Before actions
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
